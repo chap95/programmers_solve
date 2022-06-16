d71e245ca45d3a883144944a62ccc33883d3f7de
chap95 <chap95@likelion.org>
2022-06-17 00:17:07 +0900

    solving(menu_renew) add comment

--- menu_renew.js
+++ menu_renew.js
@@ -1,7 +1,7 @@
 function solution(orders, course) {
   course.map((courseLength) => {
     orders.map((order) => {
-      getCombinations(Array.from(order), courseLength);
+      getCombinations(Array.from(order), courseLength); // 배열을 얕게 복사해 새로운 배열을 만듦
     });
   });
   var answer = [];
@@ -15,11 +15,12 @@ const getCombinations = (array, selectNum) => {
     const restCombinations = getCombinations(
       origin.slice(index + 1),
       selectNum - 1
-    );
+    ); // fixed와 조합이 가능한 알파벳들의 배열을 구하는 법
     const attached = restCombinations.map((restCombination) => [
       fixed,
       ...restCombination,
     ]);
+    // 조합이 가능한 배열을 돌면서 fixed와 조합해서 최종 조합을 구하는 로직
     console.log("attached => ", attached);
     result.push(...attached);
   });
